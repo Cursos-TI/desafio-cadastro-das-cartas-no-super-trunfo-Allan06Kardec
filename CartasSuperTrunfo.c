05976b60b54c50d88620b4ef852ac683f10671ac
Allan06Kardec <223544245+Allan06Kardec@users.noreply.github.com>
2025-08-25 20:45:39 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
-
-
+// Estrutura da carta
 typedef struct {
     char estado;                 // Letra de 'A' a 'H'
-    char codigo[5];               // Ex: "A01"
-    char nomeCidade[50];          // Nome da cidade
-    int populacao;                // Número de habitantes
-    float area;                   // Área em km²
-    float pib;                    // PIB em bilhões
-    int pontosTuristicos;         // Número de pontos turísticos
+    char codigo[5];              // Ex: "A01"
+    char nomeCidade[50];         // Nome da cidade
+    int populacao;               // Número de habitantes
+    float area;                  // Área em km²
+    float pib;                   // PIB em bilhões
+    int pontosTuristicos;        // Número de pontos turísticos
+    float densidadePopulacional; // População / Área
+    float pibPerCapita;          // (PIB * 1.000.000.000) / População
 } Carta;
 
 int main() {
@@ -38,6 +40,10 @@ int main() {
 
         printf("Numero de pontos turisticos: ");
         scanf("%d", &cartas[i].pontosTuristicos);
+
+        // Calcula automaticamente os novos atributos
+        cartas[i].densidadePopulacional = cartas[i].populacao / cartas[i].area;
+        cartas[i].pibPerCapita = (cartas[i].pib * 1000000000.0) / cartas[i].populacao;
     }
 
     // Exibe as cartas
@@ -50,6 +56,8 @@ int main() {
         printf("Area: %.2f km²\n", cartas[i].area);
         printf("PIB: %.2f bilhoes de reais\n", cartas[i].pib);
         printf("Numero de Pontos Turisticos: %d\n", cartas[i].pontosTuristicos);
+        printf("Densidade Populacional: %.2f hab/km²\n", cartas[i].densidadePopulacional);
+        printf("PIB per Capita: %.2f reais\n", cartas[i].pibPerCapita);
     }
 
     return 0;
